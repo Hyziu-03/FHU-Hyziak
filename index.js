c819cbb7ee7fb4fea221003d2b34d9ec5fc036e7
Szymon Hyziak <szymon.hyziak@gmail.com>
2022-09-16 18:14:58 +0200

    Dodanie stylów i mechaniki strzałki
    Patrz: commit dcc47c01fd7ac4e486c05be19ee4e30fbc482b52

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 try {
     const header = document.getElementsByClassName('header')[0];
     const hours = new Date().getHours();
-    header.innerHTML = hours >= 10 && hours < 18 ? 'Jesteśmy otwarci do 17:00!' : 'Zapraszamy w godzinach 10:00 - 17:00.';
+    header.innerHTML = hours >= 10 && hours < 18 
+    ? 'Jesteśmy otwarci do 17:00!' 
+    : 'Zapraszamy w godzinach 10:00 - 17:00.';
 } catch (error) {
     console.log(error);
 }
+
+const chevron = document.getElementById("chevron");
+chevron.style.display = "none";
+
+window.addEventListener("resize", () => {
+    try {
+        if (window.innerWidth < 360) chevron.classList.add("chevron");
+        else chevron.classList.remove("chevron");
+    } catch (error) {
+        console.log(error);
+    }
+});
