305155294c2985bb61d181ff1a661ec5d23ead5e
Szymon Hyziak <szymon.hyziak@protonmail.com>
2024-09-03 13:58:42 +0200

    Zmiana oferty wynajmu

---
+++ README.md
@@ -0,0 +1,4 @@
+# FHU Hyziak
+
+Repozytorium oficjalnej strony internetowej FHU Hyziak.
+Zapraszamy na <https://fhu-hyziak.vercel.app/>!
---
+++ src/scripts/index.js
@@ -0,0 +1,15 @@
+const currHour = new Date().getHours();
+const inOpennHours = currHour > 9 && currHour < 18;
+
+const header = document.querySelector("#header");
+header.innerHTML = inOpennHours
+  ? "Jesteśmy otwarci do 16:00. Zapraszamy!"
+  : "Zapraszamy w godzinach 10:00 - 16:00";
+
+const contactInfo = document.querySelector("#contact-info");
+const map = document.querySelector("#map");
+
+if(map !== null) {
+  map.style.width = `${contactInfo.offsetWidth}px`;
+  map.style.height = `${contactInfo.offsetHeight}px`;
+}
