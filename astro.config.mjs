1b5ae9a5e5e1bbbe963c56d1a82fe09abf28d432
Hyziu-03 <szymon.hyziak@protonmail.com>
2026-07-01 12:01:33 +0200

    fix: fix Vite Vulnerable to Path Traversal in Optimized Deps .map Handling

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  vite: {
+    server: {
+      host: false,
+      fs: {
+        strict: true
+      }
+    }
+  }
 });
